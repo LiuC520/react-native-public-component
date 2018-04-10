bf373f6e2726b1879ba575ed71ff041a6f6533af
LiuC520 <674668211@qq.com>
2018-04-10 20:16:19 +0800

    上传示例

--- example/App.js
+++ example/App.js
@@ -9,29 +9,116 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  TextInput,
+  ScrollView
 } from 'react-native';
 
-import {CommonItem} from 'react-native-public-component'
+import {PublicCompoennt, dimens} from './react-native-public-component'
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
-    console.log(CommonItem)
     return (
-      <View style={styles.container}>
-        <CommonItem leftText="左侧文字，右侧箭头带下划线"  isRightArrowShow={true}  />
-        <CommonItem leftText="左侧文字，不带箭头带下划线"  isRightArrowShow={false}   />
-      </View>
+      <ScrollView contentContainerStyle={styles.container}>
+        <PublicCompoennt leftText="刘成"    />
+
+        <PublicCompoennt 
+          leftText="不带箭头"  
+          isRightArrowShow={false} 
+          containerStyle={styles.mv10}/>
+
+        <PublicCompoennt leftText="带下划线"  bottomLineLeftMargin={0}  />
+
+        <PublicCompoennt 
+          leftText="上下划线"  
+          bottomLineLeftMargin={0} 
+          hiddenTopLine={false} 
+          containerStyle={styles.mv10} />
+
+        <PublicCompoennt 
+          leftText="左侧文字，右侧箭头带下划线"  
+          leftTextStyle={styles.leftTextStyle}
+        />
+
+        <PublicCompoennt 
+          leftText="左侧文字 右侧图片"  
+          leftTextStyle={styles.leftTextStyle}
+          rightIcon={require('./images/head_default.png')}
+          rightIconStyle={styles.rightIconStyle1}
+          containerStyle={styles.mv10} 
+        />
+
+        <PublicCompoennt 
+          leftText="左侧文字图片 右侧图片"  
+          leftTextStyle={styles.leftTextStyle}
+          rightIcon={require('./images/head_default.png')}
+          leftIcon={require('./images/head_default.png')}
+          leftIconStyle={styles.rightIconStyle1}
+          rightIconStyle={styles.rightIconStyle1}
+          containerStyle={styles.mv10} 
+        />
+
+        <PublicCompoennt 
+          leftText="左侧文字图片 右侧文字图片"  
+          rightText="右侧文字"
+          leftTextStyle={styles.leftTextStyle}
+          rightTextStyle={styles.leftTextStyle}
+          leftComponentStyle={styles.flex2}
+          rightIcon={require('./images/head_default.png')}
+          leftIcon={require('./images/head_default.png')}
+          isRightArrowShow={false} 
+          hiddenBottomLine={true}
+          leftIconStyle={styles.rightIconStyle1}
+          rightIconStyle={styles.rightIconStyle1}
+          containerStyle={styles.mv10} 
+        />
+
+        <PublicCompoennt leftText="账号" rightComponent={this.rightLogin()} />
+
+      </ScrollView>
     );
+
   }
+  rightLogin=()=>(
+    <TextInput 
+      style={[styles.textinput,styles.mv10]}
+      placeholder = "请输入姓名" 
+      clearButtonMode='always'
+      blurOnSubmit= {false} 
+      placeholderTextColor='#bbbbbb'
+      underlineColorAndroid="transparent" 
+      numberOfLines={1}
+      ellipsizeMode='tail'
+      returnKeyType='next'
+      clearButtonMode='always'/>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+  },
+  flex2:{
+    flex:2
+  },
+  mv10:{
+    marginVertical: 10,
+  },
+  leftTextStyle:{
+    fontSize: dimens.normalize(12),
+    color:'gray'
+  },
+  rightIconStyle1:{
+    width:30,
+    height:30
+  },
+  textinput:{
+    flex:1,
+    marginRight: 16,
+    textAlign: 'right',
+    
   },
 });
